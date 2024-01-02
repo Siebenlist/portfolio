170f98976973ab0ece53d8e6927be3911d8b2f1c
Santiago Nicolas Rodriguez <siebenlist.sr@gmail.com>
2024-01-02 13:54:15 -0300

    Changed imports

--- src/app/projects/1/page.jsx
+++ src/app/projects/1/page.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ExternalLink from "@/components/ExternalLink";
-import movieRThumbnail from "../../../assets/images/movieRThumbnail.png";
+import movieRThumbnail from "@/assets/images/MovieRThumbnail.png";
 import Image from "next/image";
 
 const Page = () => {
